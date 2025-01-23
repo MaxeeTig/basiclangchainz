44f6a3c551e2860771d2aa8e18788c623d056c9b
MikArKon <mikarkon@gmail.com>
2025-01-23 18:16:04 +0300

    cluster_python_code_pipeline.py: adds graph display in chat

--- openwebui/graph_python_code_pipeline.py
+++ openwebui/graph_python_code_pipeline.py
@@ -37,7 +37,7 @@ class QueryOutput(TypedDict):
 db_config = {
     'user': 'svbo',
     'password': 'svbopwd',
-    'host': 'localhost',
+    'host': 'mysql-engine',
     'database': 'botransactions'
 }
 
